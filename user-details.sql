16aa0fc88eaba02efafb0cec3dea6ddd5576258f
Tanishq Bandari <118069212+tanisonn@users.noreply.github.com>
2025-03-05 00:56:43 +0530

    Update user-details.sql

--- user-details.sql
+++ user-details.sql
@@ -1,9 +1,3 @@
-create database user_details;
-use user_details;
-CREATE TABLE users (id bigint NOT NULL AUTO_INCREMENT, email varchar(255) DEFAULT NULL, first_name varchar(255) DEFAULT NULL, last_name varchar(255) DEFAULT NULL, PRIMARY KEY (id)) ENGINE=InnoDB AUTO_INCREMENT=106 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-
-
 
 
 create database user_details;
